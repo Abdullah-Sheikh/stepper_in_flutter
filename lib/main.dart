ee2c8dc4b5deb196c2db32ee0a662e2b2d7c8323
Abdullah <abdullahsh123@hotmail.com>
2021-04-25 02:18:44 +0500

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,60 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+
+  List<Step> _steps;
+  int _current ;
+
+  @override
+  void initState() {
+    _current = 0;
+    _steps= <Step>[
+      new Step(title: new Text("Step 1"), content: new Text("Do something"), isActive: true),
+      new Step(title: new Text("Step 2"), content: new Text("Do something"), isActive: true),
+      new Step(title: new Text("Step 3"), content: new Text("Do something"), isActive: true),
+    ];
+  }
+
+  void _stepcontinue()
+  {
+    setState(() {
+      _current++;
+      if(_current>=_steps.length) _current= _steps.length - 1;
+    });
+  }
+
+  void _stepcancel()
+  {
+    setState(() {
+      _current--;
+      if(_current < _steps.length) _current= _steps.length - 1;
+    });
+  }
+  void _steptap(int index)
+  {
+    setState(() {
+      _current= index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('My First App'),
+        title: new Text('Stepper '),
       ),
       body: new Container(
         padding: new EdgeInsets.all(32.0),
-        child: new Column(
-          children: <Widget>[
-            new Text('Abdullah Sheikh')
-          ],
+        child: new Center(
+          child: new Stepper(steps: _steps,
+            type: StepperType.vertical,
+            currentStep: _current,
+            onStepCancel: _stepcancel,
+            onStepContinue: _stepcontinue,
+            onStepTapped: _steptap,
+
+          ),
+
         ),
       ),
     );
